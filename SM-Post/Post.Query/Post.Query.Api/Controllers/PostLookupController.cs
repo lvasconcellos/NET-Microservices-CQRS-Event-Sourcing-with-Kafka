4341de060261b78628a0819460340b5f372c8c2e
lvasconcellos <48602594+lets0402@users.noreply.github.com>
2023-04-12 09:47:31 -0300

    Implementing query controller

---
+++ SM-Post/Post.Query/Post.Query.Api/Controllers/PostLookupController.cs
@@ -0,0 +1,133 @@
+﻿using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Common.DTOs;
+using Post.Query.Api.DTOs;
+using Post.Query.Api.Queries;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PostLookupController : ControllerBase
+    {
+        private readonly ILogger<PostLookupController> _logger;
+        private readonly IQueryDispatcher<PostEntity> _queryDispatcher;
+
+        public PostLookupController(ILogger<PostLookupController> logger, IQueryDispatcher<PostEntity> queryDispatcher)
+        {
+            _logger = logger;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllPostsAsync()
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindAllPostQuery());
+                return NormalResponse(posts);
+            }
+            catch (Exception exception)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to retrieve all posts!";
+                return ErrorResponse(exception, SAFE_ERROR_MESSAGE);
+            }     
+           
+        }
+
+        [HttpGet("byId/{postId}")]
+        public async Task<ActionResult> GetPostsByIdAsync(Guid postId)
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostByIdQuery { Id = postId });
+                if (posts == null || !posts.Any())
+                    return NoContent();
+
+                return Ok(new PostLookupResponse
+                {
+                    Posts = posts,
+                    Message = $"Succefully returned post"
+                });
+            }
+            catch (Exception exception)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to retrieve posts by Id!";
+                return ErrorResponse(exception, SAFE_ERROR_MESSAGE);
+            }
+
+        }
+
+        [HttpGet("byAuthor/{author}")]
+        public async Task<ActionResult> GetPostsByAuthorAsync(string author)
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostsByAuthorQuery { Author = author });
+                return NormalResponse(posts);
+            }
+            catch (Exception exception)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to retrieve posts by author!";
+                return ErrorResponse(exception, SAFE_ERROR_MESSAGE);
+            }
+
+        }
+
+        [HttpGet("withComments")]
+        public async Task<ActionResult> GetPostsWithCommentAsync()
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostsWithCommentsQuery());
+                return NormalResponse(posts);
+            }
+            catch (Exception exception)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to retrieve posts with comments!";
+                return ErrorResponse(exception, SAFE_ERROR_MESSAGE);
+            }
+
+        }
+
+        [HttpGet("withLikes/{numberoflikes}")]
+        public async Task<ActionResult> GetPostsWithLikesAsync(int numberoflikes)
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostsWithLikesQuery { NumberOfLikes = numberoflikes });
+                return NormalResponse(posts);
+            }
+            catch (Exception exception)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request to retrieve posts with likes!";
+                return ErrorResponse(exception, SAFE_ERROR_MESSAGE);
+            }
+
+        }
+
+        private ActionResult ErrorResponse(Exception exception, string safeErrorMessage)
+        {
+            _logger.Log(LogLevel.Error, exception, safeErrorMessage);
+
+            return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+            {
+                Message = safeErrorMessage
+            });
+        }
+
+        private ActionResult NormalResponse(List<PostEntity> posts)
+        {
+            if (posts == null || !posts.Any())
+                return NoContent();
+
+            var count = posts.Count();
+            return Ok(new PostLookupResponse
+            {
+                Posts = posts,
+                Message = $"Succefully returned {count} post{(count > 1 ? "s" : string.Empty)}"
+            });
+        }
+    }
+}
---
+++ SM-Post/Post.Query/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries
+{
+    public class QueryHandler : IQueryHandler
+    {
+        private readonly IPostRepository _postRepository;
+
+        public QueryHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindAllPostQuery query)
+        {
+            return await _postRepository.ListAllAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query)
+        {
+            var posts = await _postRepository.GetByIdAsync(query.Id);
+            return new List<PostEntity> { posts };
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsWithCommentsQuery query)
+        {
+            return await _postRepository.ListWithCommentsAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsWithLikesQuery query)
+        {
+            return await _postRepository.ListWithLikesAsync(query.NumberOfLikes);
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostsByAuthorQuery query)
+        {
+            return await _postRepository.ListByAuthorAsync(query.Author);
+        }
+    }
+}
